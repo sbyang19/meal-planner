19e6a9d5ac03bb9cb7f6c0f61d1401f1697be7ea
Sangbeom Yang <sbyang19@gmail.com>
2022-09-21 16:30:24 -0700

    Improved styling of most pages

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -12,8 +12,6 @@ class MealPlansController < ApplicationController
   end
 
   def new
-    puts params[:start_date]
-    puts params[:end_date]
     @meal_plan = current_user.meal_plans.build(
       start_date: params[:start_date] || Date.today,
       end_date: params[:end_date] || 6.days.from_now.to_date,
